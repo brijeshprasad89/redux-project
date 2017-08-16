184da8c3a9a37b917314dc78115a1103b9b09aa5
Brijesh Prasad <brijeshp@ush200004.na.corplan.net>
2017-08-16 12:10:15 +0530

    Revert "2"
    This reverts commit a4c1bd5ebd1c84c5df2ce03150cb082592273d38.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React-redux-demo-2</h2>
+          <h2>Welcome to React-redux-demo-1</h2>
         </div>
       </div>
     );
